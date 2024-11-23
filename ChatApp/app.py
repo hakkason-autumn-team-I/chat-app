44d66a52c8ae27913703847be7d68327db1f254a
Takanori <8398080t@gmail.com>
2024-11-23 18:31:57 +0900

    チャンネルを新規作成

--- ChatApp/app.py
+++ ChatApp/app.py
@@ -9,15 +9,35 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST'])
-def get_all_channels():
-     #ユーザーのid取得
+@app.route('/create-channel',methods=['GET','POST'])
+def create_channel():
      uid = session.get("uid")
+     if request.method == 'POST':
+          #チェンネルのidを生成
+          cid = uuid.uuid4()
 
-     #チャンネル情報取得
-     channels = dbconnect.get_all_channels(uid)
+          #htmlで入力された情報を取得
+          channel_name = request.form.get('channnel_Name')
+          event_date = request.form.get('event_date')         
+          url = request.form.get('url')         
+          uids = request.form.getlist('uids')
 
-     return render_template('index.html',channels=channels)
+          #画像保存
+          image = request.files['Event_Image']
+          image_id = uuid.uuid4()
+          image_place = os.path.join("static/user-img",str(image_id)) + ".jpg"
+          image.save(image_place)
+
+          #チャンネル情報追加
+          dbconnect.create_channel(cid,channel_name,event_date,url,image_place,uid)
+          dbconnect.add_channelmembers(uids,cid)
+
+          return redirect('/')
+     
+     #ログインしているユーザー以外のユーザーを取得
+     uids = dbconnect.all_get_other_user(uid)
+
+     return render_template('create-channel.html',uids=uids)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
