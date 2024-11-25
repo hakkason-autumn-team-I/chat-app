3584c617aa282cf403f17c3fd6eb23bdebc3ccc2
Takanori <8398080t@gmail.com>
2024-11-25 19:04:15 +0900

    モデル.py チャンネルのメンバーを取得機能追加

--- ChatApp/models.py
+++ ChatApp/models.py
@@ -76,6 +76,21 @@ class dbconnect:
         finally:
             cur.close()
 
+    #チャンネルメンバーの取得
+    def get_channelmembers(cid):
+        try:
+            conn = DB.getConnection()
+            cur = conn.cursor()
+            sql = "SELECT * FROM channelmembers WHERE not cid = %s;"
+            cur.execute(sql,(cid))
+            check_uids = cur.fetchall()
+            return check_uids
+        except Exception as e:
+            print(f'エラーが発生:{e}')
+            abort(500)
+        finally:
+            cur.close()
+
     #チャンネルメンバー削除
     def delete_channelmembers(cid):
         try:
@@ -157,7 +172,7 @@ class dbconnect:
             cur.execute(sql,(channel_name,event_date,url,image_place,cid))
             conn.commit()           
         except Exception as e:
-            print(f'ーが発生:{e}')
+            print(f'エラーが発生:{e}')
         finally:
             cur.close()
 
